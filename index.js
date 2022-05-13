159a0ecccb9a48636e92f9477ab8f108829d92af
marios <mgeorgak859@gmail.com>
2022-05-13 13:39:23 +0300

    exercise 26

--- index.js
+++ index.js
@@ -15,3 +15,29 @@ console.log(objectNumberOne)
 delete objectNumberOne.email
 console.log(objectNumberOne)
 */
+
+/*
+24 let arrayNumberOne = ["1","2","3","4","5","6","7","8","9","10"]
+25 for (let i=0; i< arrayNumberOne.length; i++){
+    console.log(arrayNumberOne[i])
+}
+*/
+
+/* 26
+function randomNumber() {
+    let arrayOfRandomNumbers = []
+    
+
+for (let i=0; i<100 ; i++)
+{
+    let randomNum = Math.floor((Math.random() * 100) + 1);
+    arrayOfRandomNumbers[i] = randomNum
+    
+    
+}
+console.log(arrayOfRandomNumbers)
+}
+randomNumber()
+
+*/
+
