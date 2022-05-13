c6558c1704fc96267489462e45604aa0da83fe89
marios <mgeorgak859@gmail.com>
2022-05-13 12:53:26 +0300

    exercises 21 & 22 & 23

---
+++ index.js
@@ -0,0 +1,17 @@
+/* 21
+let x = "John"
+let y = "Doe"
+console.log(x , "<>" ,y) */
+
+/* 22 
+let objectNumberOne = {
+    name: "name",
+    surname: "surname",
+    email: "email"
+}
+console.log(objectNumberOne)
+
+23
+delete objectNumberOne.email
+console.log(objectNumberOne)
+*/
